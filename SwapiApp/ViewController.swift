5c6a46ed19132ed16b32502aeb866a58f63fdf00
Ricardo González Pacheco <rigonpa@gmail.com>
2020-08-31 17:28:54 +0200

    Initial Commit

---
+++ SwapiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwapiApp
+//
+//  Created by Ricardo González Pacheco on 31/08/2020.
+//  Copyright © 2020 Ricardo González Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
